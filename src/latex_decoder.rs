7921359ee0415a7ea5644991bafb04247760b765
Ossi Räisä <ossi.raisa@gmail.com>
2020-11-14 19:02:44 +0200

    Use Logos library for parsing

--- src/latex_decoder.rs
+++ src/latex_decoder.rs
@@ -1,65 +1,121 @@
 use unicode_normalization::UnicodeNormalization;
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token<'a> {
+    #[regex(r"[^\{}]", |lex| lex.slice())]
+    Char(&'a str),
+
+    #[regex("[{}]")]
+    Brace,
+
+    #[regex(r#"\\`[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0300}'))]
+    Grave(String),
+
+    #[regex(r#"\\'[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0301}'))]
+    Acute(String),
+
+    #[regex(r#"\\\^[\{\\]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0302}'))]
+    Circumflex(String),
+
+    #[regex(r#"\\"[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0308}'))]
+    Umlaut(String),
+
+    #[regex(r#"\\H[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{030B}'))]
+    HungarianUmlaut(String),
+
+    #[regex(r#"\\~[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0303}'))]
+    Tilde(String),
+
+    #[regex(r#"\\c[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0327}'))]
+    Cedilla(String),
+
+    #[regex(r#"\\k[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0328}'))]
+    Ogonek(String),
+
+    #[regex(r#"\\l"#, |_| String::from("\u{0142}"))]
+    Barredl(String),
+
+    #[regex(r#"\\=[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0304}'))]
+    Macron(String),
+
+    #[regex(r#"\\b[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0332}'))]
+    BarUnder(String),
+
+    #[regex(r#"\\\.[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0307}'))]
+    DotOver(String),
+
+    #[regex(r#"\\d[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0323}'))]
+    DotUnder(String),
+
+    #[regex(r#"\\r[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{030A}'))]
+    RingOver(String),
+
+    #[regex(r#"\\u[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0306}'))]
+    Breve(String),
+
+    #[regex(r#"\\v[\{\\}]*[a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{030C}'))]
+    Caron(String),
+
+    #[regex(r#"\\t[\{\\}]*[a-zA-Z][a-zA-Z]"#, |lex| add_diacritic(lex.slice(), '\u{0308}'))]
+    Tie(String),
+
+    #[regex(r#"\\\{*o"#, |_| String::from("\u{00F8}"))]
+    SlashedO(String),
+
+    #[regex(r#"\\aa"#, |_| String::from("å"))]
+    RingOverA(String),
+    
+    #[regex(r#"\\."#, |lex| String::from(&lex.slice()[1..]))]
+    Backslash(String),
+
+    #[error]
+    Error,
+}
+
+fn add_diacritic<'c, 'd>(slice: &str, combiner: char) -> String {
+    let mut result = String::new();
+    result.push_str(&slice[slice.len() - 1..slice.len()]);
+    result.push(combiner);
+    return result
+}
 
 pub fn decode_latex(latex: &str) -> String {
     let mut result = String::new();
-    let mut iter = latex.chars();
+    let mut lex = Token::lexer(latex);
     loop {
-        let c_opt = iter.next();
-        match c_opt {
-            Some(c) => match c {
-                '{' => (),
-                '}' => (),
-                '\\' => match iter.next() {
-                    Some(c_next) => match c_next {
-                        '"' => push_diacritic('\u{0308}', &mut iter, &mut result),
-                        '\'' => push_diacritic('\u{0301}', &mut iter, &mut result),
-                        '`' => push_diacritic('\u{0300}', &mut iter, &mut result),
-                        '^' => push_diacritic('\u{0302}', &mut iter, &mut result),
-                        'H' => push_diacritic('\u{030B}', &mut iter, &mut result),
-                        '~' => push_diacritic('\u{0303}', &mut iter, &mut result),
-                        'c' => push_diacritic('\u{0327}', &mut iter, &mut result),
-                        'k' => push_diacritic('\u{0328}', &mut iter, &mut result),
-                        // 'l' => push_diacritic('\u{0308}', &mut iter, &mut result),
-                        '=' => push_diacritic('\u{0304}', &mut iter, &mut result),
-                        'b' => push_diacritic('\u{0332}', &mut iter, &mut result),
-                        '.' => push_diacritic('\u{0307}', &mut iter, &mut result),
-                        'd' => push_diacritic('\u{0323}', &mut iter, &mut result),
-                        'r' => push_diacritic('\u{030A}', &mut iter, &mut result),
-                        'u' => push_diacritic('\u{0306}', &mut iter, &mut result),
-                        'v' => push_diacritic('\u{030C}', &mut iter, &mut result),
-                        // 't' => push_diacritic('\u{0308}', &mut iter, &mut result),
-                        // 'o' => push_diacritic('\u{0308}', &mut iter, &mut result),
-                        'a' => (),//push_diacritic('\u{0308}', &mut iter, &mut result),
-                        // TODO: support for \i and \j
-                        _ => result.push(c_next),
-                    },
-                    None => break
-                }
-                _ => result.push(c)
-            },
+        match lex.next() {
+            Some(Token::Char(c)) => result.push_str(c),
+            Some(Token::Brace) => (),
+            // Some(Token::Combiner(comb)) => (),
+            Some(Token::Grave(s)) => result.push_str(&s),
+            Some(Token::Acute(s)) => result.push_str(&s),
+            Some(Token::Circumflex(s)) => result.push_str(&s),
+            Some(Token::Umlaut(s)) => result.push_str(&s),
+            Some(Token::HungarianUmlaut(s)) => result.push_str(&s),
+            Some(Token::Tilde(s)) => result.push_str(&s),
+            Some(Token::Cedilla(s)) => result.push_str(&s),
+            Some(Token::Ogonek(s)) => result.push_str(&s),
+            Some(Token::Barredl(s)) => result.push_str(&s),
+            Some(Token::Macron(s)) => result.push_str(&s),
+            Some(Token::BarUnder(s)) => result.push_str(&s),
+            Some(Token::DotOver(s)) => result.push_str(&s),
+            Some(Token::DotUnder(s)) => result.push_str(&s),
+            Some(Token::RingOver(s)) => result.push_str(&s),
+            Some(Token::RingOverA(s)) => result.push_str(&s),
+            Some(Token::Breve(s)) => result.push_str(&s),
+            Some(Token::Caron(s)) => result.push_str(&s),
+            Some(Token::Tie(s)) => result.push_str(&s),
+            Some(Token::SlashedO(s)) => result.push_str(&s),
+            // Some(Token::Umlaut(s)) => result.push_str(&s),
+            // Some(Token::Command(command)) => (),
+            Some(Token::Backslash(s)) => result.push_str(&s),
+            Some(Token::Error) => (),
             None => break
         }
     }
-    return result.nfc().collect::<String>();
-}
 
-fn push_diacritic(combiner: char, iter: &mut std::str::Chars, result: &mut String) {
-    match iter.next() {
-        Some(c) => match c {
-            '{' => match iter.next() {
-                Some(c) => {
-                    result.push(c);
-                    result.push(combiner);
-                },
-                None => ()
-            },
-            _=> {
-                result.push(c);
-                result.push(combiner);
-            },
-        },
-        None => ()
-    }
+    return result.nfc().collect::<String>();
 }
 
 #[test]
@@ -174,11 +230,9 @@ fn decode_circumflex() {
 }
 
 #[test]
-#[ignore]
 fn decode_long_umlaut() {
     let result = decode_latex(r"Hello \Ho");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    assert_eq!(result, "Hello o\u{030B}".nfc().collect::<String>());
 }
 
 #[test]
@@ -188,85 +242,66 @@ fn decode_tilde() {
 }
 
 #[test]
-#[ignore]
 fn decode_cedilla() {
     let result = decode_latex(r"Hello \cc");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    assert_eq!(result, "Hello c\u{0327}".nfc().collect::<String>());
 }
 
 #[test]
-#[ignore]
 fn decode_ogonek() {
     let result = decode_latex(r"Hello \ka");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    assert_eq!(result, "Hello a\u{0328}".nfc().collect::<String>());
 }
 
 #[test]
-#[ignore]
 fn decode_barred() {
     let result = decode_latex(r"Hello \l");
-    assert_eq!(result, "Hello ");
+    assert_eq!(result, "Hello \u{0142}".nfc().collect::<String>());
     //TODO: find correct character
 }
 #[test]
-#[ignore]
 fn decode_macron() {
     let result = decode_latex(r"Hello \=o");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    assert_eq!(result, "Hello o\u{0304}".nfc().collect::<String>());
 }
 #[test]
-#[ignore]
 fn decode_overdot() {
-    let result = decode_latex(r"Hello \-");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    let result = decode_latex(r"Hello \.a");
+    assert_eq!(result, "Hello a\u{0307}".nfc().collect::<String>());
 }
 #[test]
-#[ignore]
 fn decode_underdot() {
-    let result = decode_latex(r"Hello \d");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    let result = decode_latex(r"Hello \da");
+    assert_eq!(result, "Hello a\u{0323}".nfc().collect::<String>());
 }
 #[test]
-#[ignore]
 fn decode_breve() {
-    let result = decode_latex(r"Hello \u");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    let result = decode_latex(r"Hello \uo");
+    assert_eq!(result, "Hello o\u{0306}".nfc().collect::<String>());
 }
 #[test]
-#[ignore]
 fn decode_caron() {
-    let result = decode_latex(r"Hello \v");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    let result = decode_latex(r"Hello \vs");
+    assert_eq!(result, "Hello s\u{030C}".nfc().collect::<String>());
 }
 #[test]
 #[ignore]
 fn decode_tie() {
     let result = decode_latex(r"Hello \t");
-    assert_eq!(result, "Hello ");
+    assert_eq!(result, "Hello ".nfc().collect::<String>());
     //TODO: find correct character
 }
 #[test]
-#[ignore]
 fn decode_slashed() {
     let result = decode_latex(r"Hello \o");
-    assert_eq!(result, "Hello ");
-    //TODO: find correct character
+    assert_eq!(result, "Hello \u{00F8}");
 }
 #[test]
-#[ignore]
 fn decode_dotless_i() {
     let result = decode_latex(r"Hello \^{\i}");
     assert_eq!(result, "Hello î");
 }
 #[test]
-#[ignore]
 fn decode_dotless_j() {
     let result = decode_latex(r"Hello \^{\j}");
     assert_eq!(result, "Hello ĵ");
@@ -287,6 +322,12 @@ fn decode_chars_after_diacritic() {
 #[test]
 fn decode_diacritic_in_braces() {
     let result = decode_latex(r"Hello \'{o}");
+    assert_eq!(result, "Hello ó");
+}
+
+#[test]
+fn decode_double_brace() {
+    let result = decode_latex(r"Hello \'{{o}}");
     assert_eq!(result, "Hello ó");
 }
 
